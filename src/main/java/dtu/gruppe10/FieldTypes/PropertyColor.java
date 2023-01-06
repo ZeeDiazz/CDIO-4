bd78df1d61de590264813e2785adbf2f430e73d5
Zigalow <s224302@dtu.dk>
2023-01-06 23:08:28 +0100

    Tilføjet forskellige enum'en PropertyColor og tilføjet forskellige farver jævnfør GUI-Boardet

---
+++ src/main/java/dtu/gruppe10/FieldTypes/PropertyColor.java
@@ -0,0 +1,14 @@
+package dtu.gruppe10.FieldTypes;
+
+public enum PropertyColor {
+    ORANGE,
+    RED,
+    GREEN,
+    YELLOW,
+    GREY,
+    PURPLE,
+    TURQUOISE,
+    PINK,
+    WHITE,
+    BLACK
+}
