2ee2f1d29b09f1eb7fd54d0d116031eae9bf63ee
Daniel Weper Jensen <danielweper@gmail.com>
2023-01-06 16:43:42 +0100

    Setup Maven

---
+++ src/main/java/dtu/gruppe10/App.java
@@ -0,0 +1,13 @@
+package dtu.gruppe10;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
