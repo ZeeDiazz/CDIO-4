91e57cfa2e381e8da72989f8fcbe01ccd6667259
Zigalow <s224302@dtu.dk>
2023-01-07 00:33:04 +0100

    Tilføjet contructor og getBalance til Account-klassen

--- src/main/java/dtu/gruppe10/Account.java
+++ src/main/java/dtu/gruppe10/Account.java
@@ -1,4 +1,13 @@
 package dtu.gruppe10;
 
 public class Account {
+    private int balance;
+
+    public Account(int balance) {
+        this.balance = balance;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
 }
