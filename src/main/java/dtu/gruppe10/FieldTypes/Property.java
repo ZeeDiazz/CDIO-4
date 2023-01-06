424c138347f7112a227a35fb061313daba9defe1
Zigalow <s224302@dtu.dk>
2023-01-06 23:10:38 +0100

    Lavet contructor for klassen Property

--- src/main/java/dtu/gruppe10/FieldTypes/Property.java
+++ src/main/java/dtu/gruppe10/FieldTypes/Property.java
@@ -1,7 +1,25 @@
 package dtu.gruppe10.FieldTypes;
 
-public class Property extends Field{
+import dtu.gruppe10.Player;
 
-    
+public class Property extends Field {
+
+    private int price;
+    private PropertyColor color;
+    private boolean isSizable;          //Om der kan bygges huse på ejendommen. Hvis falsk, så nej, hvis sand, så ja
+    private boolean isBought;           //Om ejendommen er købt. Hvis falsk, så nej, hvis sand, så ja
+    private Player owner;
+
+    public Property(String name, int price, PropertyColor color, boolean isSizable) {
+        this.name = name;
+        this.price = price;
+        this.color = color;
+        this.isSizable = isSizable;
+    }
+
+
 
 }
+
+
+
