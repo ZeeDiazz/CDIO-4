6106a637be42e1f6e5e322d012daa3a17cdb4f47
Zigalow <s224302@dtu.dk>
2023-01-06 22:36:44 +0100

    Tilføjet klasserne Account, Field og Player, samt Junit testen PropertyConditionsTest

---
+++ src/main/java/dtu/gruppe10/Player.java
@@ -0,0 +1,14 @@
+package dtu.gruppe10;
+
+public class Player {
+    private String name;
+    private Account balance;
+    private int ID;
+    private boolean isBankrupt;
+
+    public Player(String name, Account balance, int ID) {
+        this.name = name;
+        this.balance = balance;
+        this.ID = ID;
+    }
+}
---
+++ src/test/java/dtu/gruppe10/PropertyConditionsTest.java
@@ -0,0 +1,11 @@
+package dtu.gruppe10;
+
+import org.junit.Test;
+
+public class PropertyConditionsTest {
+
+ //   @Test
+
+
+
+}
