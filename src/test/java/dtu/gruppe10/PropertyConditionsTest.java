c6bf971060475bd4c03e7d667ab986ccb8d92138
Zigalow <s224302@dtu.dk>
2023-01-07 15:16:33 +0100

    Lavet to test. Den ene tjekker at en property ikke har nogen ejer til at starte med, mens den anden tjekker, at huslejen er lig med den leje man skriver ind. Begge er blevet udført, og succesfulde.
    Dog kan jeg ikke få "@Before" til at fungere korrekt

--- src/test/java/dtu/gruppe10/PropertyConditionsTest.java
+++ src/test/java/dtu/gruppe10/PropertyConditionsTest.java
@@ -1,10 +1,34 @@
 package dtu.gruppe10;
 
+import dtu.gruppe10.FieldTypes.*;
+import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class PropertyConditionsTest {
 
- //   @Test
+    @Before
+    public void initialize(){
+        Property field = new Property("Rødovrevej", 1200, PropertyColor.GREEN, 50, 250);
+    }
+
+    @Test
+    public void propertyDoesNotHaveOwnerAtFirst(){
+        Property field = new Property("Rødovrevej", 1200, PropertyColor.GREEN, 50, 250);
+        assertEquals(null, field.owner);
+    }
+
+    @Test
+    public void rentIsEqualRent0(){
+        int rent0 = 583;
+        Property field = new Property("Rødovrevej", 1200, PropertyColor.GREEN, rent0, 250);
+        assertEquals(rent0, field.getCurrentRent());
+    }
+
+
+
+
+
 
 
 
