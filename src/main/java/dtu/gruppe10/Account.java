ef76db9e365791e3403aa07a83a2120195bf4ce6
Zigalow <s224302@dtu.dk>
2023-01-07 15:11:55 +0100

    Tilføjet metoder: Sørge for leje Sætte balance Returnere om en spiller er bankerot
    Disse metoder er i Account. Der er blevet tilføjet de samme metoder i Player-klassen, så der kan refereres til disse metoder i Account-klassen, via metoderne i Player-klassen

--- src/main/java/dtu/gruppe10/Account.java
+++ src/main/java/dtu/gruppe10/Account.java
@@ -10,4 +10,22 @@ public class Account {
     public int getBalance() {
         return balance;
     }
+
+
+    public void setBalance(int amount) {
+        this.balance = this.balance + balance;
+    }
+
+    public boolean isBankrupt(Player player, int fees) {
+        return player.getBalance() - fees > 0;
+    }
+
+    public void handleRent(Player reciever, Player payer, int rent) {
+        if (!(payer.isBankrupt(rent))) {
+            reciever.setBalance(rent);
+            payer.setBalance(-rent);
+        }
+    }
+
+
 }
