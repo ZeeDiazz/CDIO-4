61a93bf5901ad7e30cbf35afd9e982ef69991e80
Zigalow <s224302@dtu.dk>
2023-01-07 15:21:14 +0100

    Testet at en property's ejer er den person som køber den. Succesfuld

--- src/test/java/dtu/gruppe10/PropertyConditionsTest.java
+++ src/test/java/dtu/gruppe10/PropertyConditionsTest.java
@@ -3,33 +3,37 @@ package dtu.gruppe10;
 import dtu.gruppe10.FieldTypes.*;
 import org.junit.Before;
 import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class PropertyConditionsTest {
 
     @Before
-    public void initialize(){
+    public void initialize() {
         Property field = new Property("Rødovrevej", 1200, PropertyColor.GREEN, 50, 250);
     }
 
     @Test
-    public void propertyDoesNotHaveOwnerAtFirst(){
+    public void propertyDoesNotHaveOwnerAtFirst() {
         Property field = new Property("Rødovrevej", 1200, PropertyColor.GREEN, 50, 250);
         assertEquals(null, field.owner);
     }
+    @Test
+    public void playerBecomesOwnerWhenBuyingProperty() {
+        Player player = new Player("Ziggy", 10000, 1);
+        Property field = new Property("Rødovrevej", 1200, PropertyColor.GREEN, 50, 250);
+        field.buyProperty(player);
+        assertTrue(field.owner == player);
+    }
+
 
     @Test
-    public void rentIsEqualRent0(){
+    public void rentIsEqualRent0() {
         int rent0 = 583;
         Property field = new Property("Rødovrevej", 1200, PropertyColor.GREEN, rent0, 250);
         assertEquals(rent0, field.getCurrentRent());
     }
-
-
-
-
-
-
 
 
 }
