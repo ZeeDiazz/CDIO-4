6040e6e869f876220be6e0181a69d6946e06f788
ZEE <Zaidsh02@gmail.com>
2023-01-07 23:01:25 +0100

    constructor in Board class for the number of Players

--- src/main/java/dtu/gruppe10/Board.java
+++ src/main/java/dtu/gruppe10/Board.java
@@ -1,5 +1,9 @@
 package dtu.gruppe10;
 
 public class Board {
+    private int[] playerIndexesOnBoard;
+    Board(int numberOfPlayers){
+        this.playerIndexesOnBoard = new int[numberOfPlayers];
+    }
 
 }
