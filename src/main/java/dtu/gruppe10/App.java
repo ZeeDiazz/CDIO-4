17d327ff37d1a8a3f1375a5e222c73af238bc902
ZEE <Zaidsh02@gmail.com>
2023-01-08 02:16:14 +0100

    Clean up in App class

--- src/main/java/dtu/gruppe10/App.java
+++ src/main/java/dtu/gruppe10/App.java
@@ -16,36 +16,6 @@ public class App {
 
         Game game = new Game(players);
 
-
-        /*//How many player?
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter the number of players (2-6):");
-        int playerCount = scan.nextInt();
-
-            // Check that player count is between 2 and 6
-            if (playerCount <= 2 || playerCount >= 6) {
-                System.out.println("Invalid number of players. Please enter a number between 2 and 6.");
-                System.out.println("Enter the number of players (2-6):");
-                playerCount = scan.nextInt();
-            }
-
-            // Create an array of players
-            Player[] players = new Player[playerCount];
-            for (int i = 0; i < playerCount; i++) {
-                players[i] = new Player("Player " + (i + 1), 20, i); // balance is 20 for now
-            }
-
-            // Create a game with the array of players
-            Game game = new Game(players);
-
-            while (true){
-                int d1 = 2;
-                int d2 = 5;
-                Player currentPlayer = game.getCurrentPlayer();
-                System.out.println(String.format("Starting %s's turn (money: %d)", currentPlayer.name, currentPlayer.getBalance()));
-
-            }*/
-
     }
     private static int getPlayerCount(){
         while (true){
