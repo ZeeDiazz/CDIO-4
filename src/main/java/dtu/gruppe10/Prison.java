5c56fd1daa6ab0d6ab473dbc571e0af0160856bd
Felix Schmidt <s224313@dtu.dk>
2023-01-08 18:42:32 +0800

    Tilføjet metoder: isInPrison, inPrisonEffect

--- src/main/java/dtu/gruppe10/Prison.java
+++
@@ -1,7 +0,0 @@
-package dtu.gruppe10;
-
-public class Prison {
-    //TODO tilføj metode til at betale kaution eller bruge løsladerkort, hvis spilleren er i fængsel
-    //TODO tilføj metode til at betale kaution, efter en spiller har været i fængsel 3 runder (tvunget til at betale)
-    //TODO gør sådan at man ikke kan: bevæge sig og opkræve leje
-}
