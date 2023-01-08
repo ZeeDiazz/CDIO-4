48eb1d1a146b5e57521b50732b8e4d396927a5ca
Felix Schmidt <s224313@dtu.dk>
2023-01-08 18:46:12 +0800

    Tilføjet UnitTest for PrisonField

---
+++ src/test/java/dtu/gruppe10/PrisonFieldTest.java
@@ -0,0 +1,12 @@
+package dtu.gruppe10;
+
+import junit.framework.TestCase;
+
+public class PrisonFieldTest extends TestCase {
+
+    public void testIsInPrison() {
+    }
+
+    public void testInPrisonEffect() {
+    }
+}
