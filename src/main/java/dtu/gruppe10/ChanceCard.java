355c8a1fe73f791829cfe8574b2820f0495f53b8
ZEE <Zaidsh02@gmail.com>
2023-01-09 00:50:03 +0100

    ChanceCard Go To Jail - Almost working

---
+++ src/main/java/dtu/gruppe10/ChanceCard.java
@@ -0,0 +1,17 @@
+package dtu.gruppe10;
+
+
+public class ChanceCard {
+    private String text;
+
+    ChanceCard(String text){
+        this.text = text;
+    }
+
+    //Could use a interface and than make a class for everyChanceCard...
+    public void GoToPrisonAction(Player player) {
+        player.setPosition(31);
+        player.setInPrison(true);
+    }
+
+}
