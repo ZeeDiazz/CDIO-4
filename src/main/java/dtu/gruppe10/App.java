68a80c14832cd68f1148da467accfd2a8c4aa743
ZEE <Zaidsh02@gmail.com>
2023-01-08 02:15:21 +0100

    Create 2-6 players

--- src/main/java/dtu/gruppe10/App.java
+++ src/main/java/dtu/gruppe10/App.java
@@ -6,12 +6,18 @@ import java.util.Scanner;
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
+    private static Scanner scan = new Scanner(System.in);
     public static void main( String[] args ) {
 
+        int playerCount = getPlayerCount();
 
-        //How many player?
+        Player[] players = createPlayer(playerCount);
+
+        Game game = new Game(players);
+
+
+        /*//How many player?
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter the number of players (2-6):");
         int playerCount = scan.nextInt();
@@ -38,6 +44,31 @@ public class App
                 Player currentPlayer = game.getCurrentPlayer();
                 System.out.println(String.format("Starting %s's turn (money: %d)", currentPlayer.name, currentPlayer.getBalance()));
 
-            }
+            }*/
+
     }
+    private static int getPlayerCount(){
+        while (true){
+            System.out.println("Enter the number of players (2-6):");
+            try{
+                int playerCount = scan.nextInt();
+                if (playerCount >= 2 && playerCount <= 6) {
+                    return playerCount;
+                } else {
+                    System.out.println("Enter a number between 2 and 6.");
+                }
+            } catch (Exception e) {
+                System.out.println("Enter a valid number");
+                scan.next();
+            }
+        }
+    }
+    private static Player[] createPlayer(int playerCount){
+        Player[] players = new Player[playerCount];
+        for(int i = 0; i < playerCount; i++){
+            players[i] = new Player("Player " + (i + 1), 20, i);
+        }
+        return players;
+    }
+
 }
