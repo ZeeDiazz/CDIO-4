c71d0b89f8327c3c5f745b0d28e3a265264e2280
Felix Schmidt <s224313@dtu.dk>
2023-01-08 19:13:16 +0800

    UnitTest for: "i fængsel eller ej" og "ikke bevæge sig i fængsel"

--- src/test/java/dtu/gruppe10/PrisonFieldTest.java
+++ src/test/java/dtu/gruppe10/PrisonFieldTest.java
@@ -4,9 +4,41 @@ import junit.framework.TestCase;
 
 public class PrisonFieldTest extends TestCase {
 
-    public void testIsInPrison() {
+    public void testIfPlayerIsInPrison() {
+        Player player = new Player("Felix", 12345, 2);
+
+        player.setField("PrisonField");
+
+        assertEquals("PrisonField", player.getField());
+
+        assertTrue(player.isInPrison());
+
+    }
+    public void testIfPlayerIsNotInPrison(){
+        Player player = new Player ("Felix", 12345, 3);
+
+        player.setField("Rødovrevej");
+
+        AssertFalse(player.isInPrison());
     }
 
+
     public void testInPrisonEffect() {
+        Player player = new Player("Felix", 123456, 1);
+        player.setField("Prison");
+
+        field.setEffect("something");
+
+        assertEquals("something", field.getEffect());
+    }
+
+    public void testIfPlayerCanMoveInPrison() {
+        Player player = new Player ("Felix", 1234567, 3);
+
+        player.setField("Prison");
+
+        field.setEffect("NoMovement");
+
+        assertEquals("NoMovement", field.getEffect());
     }
 }
