b6e6f9b6251aa890338795cc614212675b31055e
ZEE <Zaidsh02@gmail.com>
2023-01-09 15:29:11 +0100

    Unittest SixSidedDieTest Begin

---
+++ src/test/java/dtu/gruppe10/DieLogic/SixSidedDieTest.java
@@ -0,0 +1,11 @@
+package dtu.gruppe10.DieLogic;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class SixSidedDieTest extends TestCase {
+    @Test
+    public void testRoll() {
+
+    }
+}
