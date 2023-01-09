becd30d9c63e3711548dae239618e6f4787b8d5f
Daniel Weper Jensen <danielweper@gmail.com>
2023-01-09 23:40:05 +0100

    Added a DiceRoll class

---
+++ src/main/java/dtu/gruppe10/DieLogic/DiceRoll.java
@@ -0,0 +1,35 @@
+package dtu.gruppe10.DieLogic;
+
+public class DiceRoll {
+    public final int Sum;
+    public final boolean AreSame;
+    protected int[] values;
+
+    public DiceRoll(Die... dice) {
+        int firstFace = dice[0].getFace();
+        int sum = firstFace;
+
+        boolean areAllSame = true;
+
+        this.values = new int[dice.length];
+        this.values[0] = firstFace;
+        for (int i = 1; i < dice.length; ++i) {
+            Die die = dice[i];
+
+            int dieFace = die.getFace();
+
+            sum += dieFace;
+            if (firstFace != dieFace) {
+                areAllSame = false;
+            }
+            this.values[i] = dieFace;
+        }
+
+        this.Sum = sum;
+        this.AreSame = areAllSame;
+    }
+
+    public int getValue(int index) {
+        return values[index];
+    }
+}
