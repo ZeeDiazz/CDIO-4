e31a2d188be6f4bffc6841145a2fe8021c63811b
Daniel Weper Jensen <danielweper@gmail.com>
2023-01-09 23:35:30 +0100

    Fixed formatting in DieCup

--- src/main/java/dtu/gruppe10/DieLogic/DieCup.java
+++ src/main/java/dtu/gruppe10/DieLogic/DieCup.java
@@ -2,25 +2,24 @@ package dtu.gruppe10.DieLogic;
 
 //Taget fra CDIO 3
 public class DieCup {
-        public final Die Die1;
-        public final Die Die2;
+    public final Die Die1;
+    public final Die Die2;
 
-        public DieCup(Die die1, Die die2) {
-            this.Die1 = die1;
-            this.Die2 = die2;
-        }
+    public DieCup(Die die1, Die die2) {
+        this.Die1 = die1;
+        this.Die2 = die2;
+    }
 
-        public void roll() {
-            Die1.roll();
-            Die2.roll();
-        }
+    public void roll() {
+        Die1.roll();
+        Die2.roll();
+    }
 
-        public boolean hasPair() {
-            return Die1.getFace() == Die2.getFace();
-        }
+    public boolean hasPair() {
+        return Die1.getFace() == Die2.getFace();
+    }
 
-        public int getSum() {
-            return Die1.getFace() + Die2.getFace();
-        }
-
+    public int getSum() {
+        return Die1.getFace() + Die2.getFace();
+    }
 }
