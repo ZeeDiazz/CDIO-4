2eca164e87ac677ee8afaeaf6d9cf50ddcce722b
ZEE <Zaidsh02@gmail.com>
2023-01-09 15:48:26 +0100

    Unittest DieCupTest Done

--- src/test/java/dtu/gruppe10/DieLogic/DieCupTest.java
+++ src/test/java/dtu/gruppe10/DieLogic/DieCupTest.java
@@ -17,5 +17,11 @@ public class DieCupTest extends TestCase {
     }
     @Test
     public void testGetSum() {
+        Die die1 = new SixSidedDie();
+        Die die2 = new SixSidedDie();
+        DieCup cup = new DieCup(die1,die2);
+        cup.roll();
+        int DiceSum = cup.getSum();
+        assertEquals(DiceSum,(die1.getFace() +die2.getFace()));
     }
 }
