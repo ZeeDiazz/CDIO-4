ab7dc32621e71e58fcf3b2100faed659813ca613
Felix Schmidt <s224313@dtu.dk>
2023-01-09 18:35:19 +0800

    get og set for inPrison

--- src/main/java/dtu/gruppe10/FieldTypes/GoToPrisonField.java
+++ src/main/java/dtu/gruppe10/FieldTypes/GoToPrisonField.java
@@ -1,14 +1,15 @@
 package dtu.gruppe10.FieldTypes;
 
 import dtu.gruppe10.Player;
+import dtu.gruppe10.PrisonField;
 
-public class GoToPrisonField extends Field {
-    int turnsInPrison;
+public class GoToPrisonField extends PrisonField {
 
         public void goToPrisonField(Player player) {
             player.setPosition(31); //31 because the jail is there
             player.setInPrison(true);
-            turnsInPrison = 0;
+            setTurnsInPrison(0);
+
         }
         public void landedOn(Player player) {
             goToPrisonField(player);
