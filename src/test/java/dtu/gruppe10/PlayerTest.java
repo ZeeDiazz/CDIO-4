f1bd79510924abdd4a8549e494158d84f3d64db7
Daniel Weper Jensen <danielweper@gmail.com>
2023-01-09 13:15:54 +0100

    More clean-up in tests

--- src/test/java/dtu/gruppe10/PlayerTest.java
+++ src/test/java/dtu/gruppe10/PlayerTest.java
@@ -26,10 +26,10 @@ public class PlayerTest extends TestCase {
         Player player = new Player("Player 1", 20, 0);
 
         // Set the player's balance to 100
-        player.addToBalance(100);
+        player.Account.add(100);
 
         // Check that the player's balance is now 120 (100+20)
-        assertEquals(120, player.getBalance());
+        assertEquals(120, player.Account.getBalance());
     }
     @Test
     public void testIsBankrupt() {
@@ -37,10 +37,10 @@ public class PlayerTest extends TestCase {
         Player player = new Player("Player 1", 1000, 0);
 
         // Check that the player is not bankrupt (has sufficient funds to pay a fee of 500)
-        assertFalse(player.isBankrupt(500));
+        assertFalse(player.canPay(500));
 
         // Check that the player is bankrupt (does not have sufficient funds to pay a fee of 1000)
-        assertTrue(player.isBankrupt(1000));
+        assertTrue(player.canPay(1000));
     }
     @Test
     public void testHandleRent() {
