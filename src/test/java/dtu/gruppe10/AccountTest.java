fd35ec09868a4ecea9dc01cfc6fc88fc8678a6f5
ZEE <Zaidsh02@gmail.com>
2023-01-09 18:11:55 +0100

    Unittest testGetBalance() & testIsBankrupt()

--- src/test/java/dtu/gruppe10/AccountTest.java
+++ src/test/java/dtu/gruppe10/AccountTest.java
@@ -6,9 +6,15 @@ import org.junit.Test;
 public class AccountTest extends TestCase {
     @Test
     public void testGetBalance() {
+        Account balance = new Account(30000);
+        assertEquals(30000,balance.getBalance());
     }
     @Test
     public void testIsBankrupt() {
+        Account balance = new Account(30000);
+        assertFalse(balance.isBankrupt());
+        balance = new Account(-5);
+        assertTrue(balance.isBankrupt());
     }
     @Test
     public void testAdd() {
