d08bf5feff99b7e29a29a5ae08cd4d0afec28bcf
ZEE <Zaidsh02@gmail.com>
2023-01-09 15:52:55 +0100

    Unittest AccountTest Begin

---
+++ src/test/java/dtu/gruppe10/AccountTest.java
@@ -0,0 +1,19 @@
+package dtu.gruppe10;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class AccountTest extends TestCase {
+    @Test
+    public void testGetBalance() {
+    }
+    @Test
+    public void testIsBankrupt() {
+    }
+    @Test
+    public void testAdd() {
+    }
+    @Test
+    public void testSubtract() {
+    }
+}
