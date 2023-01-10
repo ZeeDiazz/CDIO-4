8614495f6fd7c51a967e7ce5fd0d90acbe34d4a1
Felix Schmidt <s224313@dtu.dk>
2023-01-11 00:52:54 +0800

    USD1

--- src/test/java/dtu/gruppe10/dice/DieCupTest.java
+++ src/test/java/dtu/gruppe10/dice/DieCupTest.java
@@ -23,4 +23,28 @@ public class DieCupTest extends TestCase {
         int DiceSum = cup.getResult().Sum;
         assertEquals(DiceSum,(die1.getFace() +die2.getFace()));
     }
+    @Test
+    public void testIfPlayerCanThrowThreePairsInARow(){
+        TestDie die1 = new TestDie(4, 3 ,5);
+        TestDie die2 = new TestDie(4, 3, 5);
+        DieCup cup = new DieCup(die1,die2);
+        cup.roll();
+        boolean isHasPair = cup.getResult().AreSame;
+        assertTrue(isHasPair == (die1.getFace() == die2.getFace()));
+        cup.roll();
+
+        boolean isHasPair2 = cup.getResult().AreSame;
+        assertTrue(isHasPair2 == (die1.getFace() == die2.getFace()));
+
+        boolean isHasPair3 = cup.getResult().AreSame;
+        assertTrue(isHasPair3 == (die1.getFace() == die2.getFace()));
+    }
+    public void testIfPlayerCanThrowOnPair(){
+        TestDie die1 = new TestDie(4, 3 ,5);
+        TestDie die2 = new TestDie(4, 3, 5);
+        DieCup cup = new DieCup(die1,die2);
+        cup.roll();
+        boolean isHasPair = cup.getResult().AreSame;
+        assertTrue(isHasPair == (die1.getFace() == die2.getFace()));
+    }
 }
