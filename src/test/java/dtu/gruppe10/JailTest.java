0f002d340ce408391208b6d6c86b756874f8a51a
ZEE <Zaidsh02@gmail.com>
2023-01-10 15:28:08 +0100

    Unit-test jail begin

--- src/test/java/dtu/gruppe10/JailTest.java
+++ src/test/java/dtu/gruppe10/JailTest.java
@@ -2,40 +2,47 @@ package dtu.gruppe10;
 
 import junit.framework.TestCase;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class JailTest extends TestCase {
+    @Test
+    public void testAddPlayer() {
+        Jail jail = new Jail(3);
+        Player player1 = new Player(1,30000);
+        Player player2 = new Player(1,30000);
+
+        jail.addPlayer(player1);
+        assertTrue(jail.playerIsJailed(player1)); //Is player 1 in jail
+        assertFalse(jail.playerIsJailed(player2)); //Is player 2 in jail
+        assertEquals(0, jail.turnsServed(player1));
+
+
+    }
 
     @Test
-    public void addPlayer() {
+    public void testReleasePlayer() {
+
+
+    }
+
+    @Test
+    public void testPlayerIsJailed() {
 
 
 
     }
 
     @Test
-    public void releasePlayer() {
-
-
-    }
-
-    @Test
-    public void playerIsJailed() {
-
-
+    public void testTurnsServed() {
 
     }
 
     @Test
-    public void turnsServed() {
-
+    public void testPlayerHasToGetOut() {
     }
 
     @Test
-    public void playerHasToGetOut() {
-    }
-
-    @Test
-    public void playerServedTurn() {
+    public void testPlayerServedTurn() {
     }
 }
