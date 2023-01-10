fe501c784a77c9103430da45cdf4ce8c0e568a62
Zigalow <s224302@dtu.dk>
2023-01-10 02:17:29 +0100

    Tilføjet for street

--- src/main/java/dtu/gruppe10/board/fields/ArrayOfFields.java
+++ src/main/java/dtu/gruppe10/board/fields/ArrayOfFields.java
@@ -1,4 +1,5 @@
 package dtu.gruppe10.board.fields;
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -24,11 +25,23 @@ public class ArrayOfFields {
         // Load the fields.CSV
         BufferedReader reader = new BufferedReader(new FileReader("fields.CSV"));
         String textLine;
+        int counter = 0;
 
         //While there is text line in CSV
         while ((textLine = reader.readLine()) != null) {
-            String[] texts = textLine.split(","); //Spilts the text if ";"
+            String[] texts = textLine.split(";"); //Spilts the text if ";"
+
+            if (texts[2].equals("start")) {
+                this.add(new StartField(counter));
+                counter++;
+            } else if (texts[2].equals("street")) {
+                this.add(new StreetField(counter, Integer.valueOf(texts[3]), new int[]{Integer.valueOf(texts[5]), Integer.valueOf(texts[6]), Integer.valueOf(texts[7]), Integer.valueOf(texts[8])}, StreetColor.RED, 0));
+            }
+            else if (texts[2].equals("street")) {
+                this.add(new StreetField(counter, Integer.valueOf(texts[3]), new int[]{Integer.valueOf(texts[5]), Integer.valueOf(texts[6]), Integer.valueOf(texts[7]), Integer.valueOf(texts[8])}, StreetColor.RED, 0));
+
         }
+
 
     }
 
