c7294c820e08566e89abcce4f424633c14efd48a
Felix Schmidt <s224313@dtu.dk>
2023-01-10 22:04:59 +0800

    UnitTest For jail Class Started

---
+++ src/test/java/dtu/gruppe10/JailTest.java
@@ -0,0 +1,41 @@
+package dtu.gruppe10;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+public class JailTest extends TestCase {
+
+    @Test
+    public void addPlayer() {
+
+
+
+    }
+
+    @Test
+    public void releasePlayer() {
+
+
+    }
+
+    @Test
+    public void playerIsJailed() {
+
+
+
+    }
+
+    @Test
+    public void turnsServed() {
+
+    }
+
+    @Test
+    public void playerHasToGetOut() {
+    }
+
+    @Test
+    public void playerServedTurn() {
+    }
+}
