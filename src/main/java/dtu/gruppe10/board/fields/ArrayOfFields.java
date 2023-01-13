1210a4813f92a2cb9eb27598747a0867db8a6ff5
Zigalow <s224302@dtu.dk>
2023-01-13 20:18:53 +0100

    Opdateret ArrayOfFields, så den tager modifiedFields - fields.csv.

--- src/main/java/dtu/gruppe10/board/fields/ArrayOfFields.java
+++ src/main/java/dtu/gruppe10/board/fields/ArrayOfFields.java
@@ -9,8 +9,6 @@ public class ArrayOfFields {
 
     private ArrayList<Field> deckOfFields;
 
-    private Field[] fields;
-
     public ArrayOfFields() {
         this.deckOfFields = new ArrayList<>();
     }
@@ -21,51 +19,77 @@ public class ArrayOfFields {
 
     public void readFieldData() throws IOException {
         // Load the fields.CSV
-        BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/fields.CSV"));
+        BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/modifiedFields - fields.csv"));
         String textLine;
-        int counter = 0;
         int jailCounter = 0;
 
         //While there is text line in CSV
         while ((textLine = reader.readLine()) != null) {
-            String[] texts = textLine.split(","); //Splits the text if ","
+            String[] parts = textLine.split(","); //Splits the text if ","
 
-            switch (texts[2]) {
+            switch (parts[1]) {
                 case "start":
-                    this.add(new StartField(counter));
+                    this.add(new StartField(Integer.parseInt(parts[0])));
                     break;
                 case "street":
-                    this.add(new StreetField(counter, Integer.valueOf(texts[3]), new int[]{Integer.valueOf(texts[5]), Integer.valueOf(texts[6]), Integer.valueOf(texts[7]), Integer.valueOf(texts[8]), Integer.valueOf(texts[9]), Integer.valueOf(texts[10])}, StreetColor.RED, 0));
+                    this.add(new StreetField(Integer.parseInt(parts[0]), Integer.parseInt(parts[4]), Integer.parseInt(parts[5]), new int[]{Integer.parseInt(parts[6]), Integer.parseInt(parts[7]), Integer.parseInt(parts[8]), Integer.parseInt(parts[9]), Integer.parseInt(parts[10]), Integer.parseInt(parts[11])}, selectColor(parts[3]), Integer.parseInt(parts[12])));
                     break;
                 case "chance":
-                    this.add(new ChanceField(counter));
+                    this.add(new ChanceField(Integer.parseInt(parts[0])));
                     break;
                 case "ferry":
-                    this.add(new FerryField(counter, Integer.valueOf(texts[3]), new int[]{Integer.valueOf(texts[5]), Integer.valueOf(texts[6]), Integer.valueOf(texts[7]), Integer.valueOf(texts[8])}));
+                    this.add(new FerryField(Integer.parseInt(parts[0]), Integer.parseInt(parts[4]), new int[]{Integer.parseInt(parts[6]), Integer.parseInt(parts[7]), Integer.parseInt(parts[8]), Integer.parseInt(parts[9])}));
                     break;
                 case "tax":
-                    this.add(new TaxField(counter, Integer.valueOf(texts[3])));
+                    this.add(new TaxField(Integer.parseInt(parts[0]), Integer.parseInt(parts[4])));
                     break;
                 case "jail":
                     if (jailCounter == 0) {
-                        this.add(new JailField(counter));
+                        this.add(new JailField(Integer.parseInt(parts[0])));
                         jailCounter++;
                     } else {
-                        this.add(new GoToJailField(counter));
+                        this.add(new GoToJailField(Integer.parseInt(parts[0])));
                     }
                     break;
                 case "brewery":
-                    this.add(new BreweryField(counter, Integer.valueOf(texts[3]), new int[]{Integer.valueOf(texts[5]), Integer.valueOf(texts[6])}));
+                    this.add(new BreweryField(Integer.parseInt(parts[0]), Integer.parseInt(parts[4]), new int[]{Integer.parseInt(parts[6]), Integer.parseInt(parts[7])}));
                     break;
                 case "refugee":
-                    this.add(new FreeParkingField(counter));
+                    this.add(new FreeParkingField(Integer.parseInt(parts[0])));
                     break;
             }
-            counter++;
         }
     }
 
     public Field[] getFields() {
         return deckOfFields.toArray(new Field[0]);
+    }
+
+
+    public StreetColor selectColor(String color) {
+        switch (color) {
+            case "orange":
+                return StreetColor.ORANGE;
+            case "red":
+                return StreetColor.RED;
+            case "green":
+                return StreetColor.GREEN;
+            case "yellow":
+                return StreetColor.YELLOW;
+            case "grey":
+                return StreetColor.GREY;
+            case "purple":
+                return StreetColor.PURPLE;
+            case "cyan":
+                return StreetColor.CYAN;
+            case "pink":
+                return StreetColor.PINK;
+            case "white":
+                return StreetColor.WHITE;
+            case "black":
+                return StreetColor.BLACK;
+        }
+
+        return StreetColor.BLACK;
     }
 }
