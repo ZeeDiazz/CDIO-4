c432a4ba0a5141ca46c7261cb9113d37989cee9b
Zigalow <s224302@dtu.dk>
2023-01-13 14:39:26 +0100

    Tilføjet metode der tjekker om en ejer ejer alle ejendomme i settet

--- src/main/java/dtu/gruppe10/board/fields/StreetField.java
+++ src/main/java/dtu/gruppe10/board/fields/StreetField.java
@@ -1,5 +1,8 @@
 package dtu.gruppe10.board.fields;
 
+import dtu.gruppe10.App;
+import dtu.gruppe10.board.Board;
+
 public class StreetField extends PropertyField {
     public final StreetColor Color;
     public final int PropertiesInSet;
@@ -11,6 +14,21 @@ public class StreetField extends PropertyField {
         this.Color = color;
         this.houseCount = 0;
         this.PropertiesInSet = propertiesInSet;
+    }
+
+
+    public boolean ownsAllInSet() {
+        int counter = 0;
+        for (Field street : App.game.Board.getFields()) {
+            if (!(street instanceof StreetField)) {
+                continue;
+            } else {
+                if (((StreetField) street).Color == this.Color && ((StreetField) street).owner == this.owner){
+                    counter++;
+                }
+            }
+        }
+        return this.PropertiesInSet == counter;
     }
 
     @Override
@@ -36,5 +54,25 @@ public class StreetField extends PropertyField {
         }
         return false;
     }
+
+
+
+   /* // Til når man kan bygge huse
+
+    public void updateCurrentRent(int numberHouses) {
+        if (numberHouses == 1) {
+            this.currentRent = this.rent1;
+        } else if (numberHouses == 2) {
+            this.currentRent = this.rent2;
+        } else if (numberHouses == 3) {
+            this.currentRent = this.rent3;
+        } else if (numberHouses == 4) {
+            this.currentRent = this.rent4;
+        } else if (numberHouses == 5) {
+            this.currentRent = this.rent5;
+        }
+    }*/
+
+
 }
 
