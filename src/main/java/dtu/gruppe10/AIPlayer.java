4896b9eb9026e6f1f6631da704d92c8042b73bba
ZEE <Zaidsh02@gmail.com>
2023-01-16 00:47:28 +0100

    USG4 - AIPlayer Begin

---
+++ src/main/java/dtu/gruppe10/AIPlayer.java
@@ -0,0 +1,18 @@
+package dtu.gruppe10;
+
+import dtu.gruppe10.dice.DieCup;
+import dtu.gruppe10.dice.SixSidedDie;
+
+public class AIPlayer extends Player{
+
+    public AIPlayer(int id, int startingBalance) {
+        super(id, startingBalance);
+    }
+
+    //Make it owns decision:
+    public void takeTurn() {
+
+    }
+
+
+}
