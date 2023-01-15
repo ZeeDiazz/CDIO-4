99626747164385c38c117639851c526172e53786
Felix Schmidt <s224313@dtu.dk>
2023-01-15 15:33:55 +0800

    ræderi != coke/squash (fjernet chancekort der ikke skulle være der)

--- src/main/java/dtu/gruppe10/ChanceCard/ControllerChanceCard.java
+++ src/main/java/dtu/gruppe10/ChanceCard/ControllerChanceCard.java
@@ -18,14 +18,17 @@ public class ControllerChanceCard {
 
     private ControllerChanceCard() {
         chanceCards = new ParentChanceCard[]{
-                new MoveToCard(29, new int[]{39}),
-                new MoveToCard(129, new int[]{29}),
-                new MoveToCard(229, new int[]{20}),
-                new MoveToCard(329, new int[]{4}),
-                new MoveToCard(429, new int[]{12}),
-                new MoveToCard(529, new int[]{9}),
-                new MoveToCard(629, new int[]{1}),
-                new MoveToCard(729, new int[]{5, 15, 25, 35}),
+                new MoveToCard(29, new int[]{1}),
+                new MoveToCard(29, new int[]{1}),
+                new MoveToCard(129, new int[]{12}),
+                new MoveToCard(229, new int[]{25}),
+                new MoveToCard(329, new int[]{33}),
+                new MoveToCard(429, new int[]{20}),
+                new MoveToCard(529, new int[]{40}),
+
+                new MoveToCard(629, new int[]{6, 16, 26, 36}),
+                new MoveToCard(729, new int[]{13,29}),
+                new MoveToCard(829, new int[]{16}),
                 new GoToJailCard(40),
                 new MoveCard(31,3),
                 new MoveCard(32,-3)
@@ -38,92 +41,91 @@ public class ControllerChanceCard {
         * cards and returning it. The method takes a player_iD as an argument, it shifts all the
         * elements of the array to the left, and moves the first element to the last, so the element
         * that is at the last position of the array is the one that is drawn.*/
-        public ParentChanceCard draw(int player_iD) {
+        public ParentChanceCard draw(int playerId) {
             ParentChanceCard upper = chanceCards[0];
             for (int i = 0; i < chanceCards.length - 1; i++) {
                 chanceCards[i] = chanceCards[i + 1];
             }
             chanceCards[chanceCards.length - 1] = upper;
-            int destination = 0;
-            int moveToField = 0;
+            int amount = 0;
+            int newPos = 0;
+
 
             if (upper instanceof MoveToCard) {
                 switch (upper.getID()) {
                     case 29: // start
-                        destination = 1;
+                        newPos = 1;
                         break;
                     case 129: // frederiksberg alle
-                        destination = 12;
+                        newPos = 12;
                         break;
                     case 229: // grønningen
-                        destination = 20;
+                        newPos = 25;
                         break;
                     case 329: // hvimmelskaftet
-                        destination = 33;
+                        newPos = 33;
                         break;
                     case 429: // strandvejen
-                        destination = 20;
+                        newPos = 20;
                         break;
                     case 529: //rådhuspladsen
-                        destination = 40;
+                        newPos = 40;
                         break;
                     case 629: // nærmeste færge
-                        if(board.getPlayerPosition(player_iD)==3 || board.getPlayerPosition(player_iD)==37) {
-                            destination = 6;
-                        }else if(board.getPlayerPosition(player_iD)==8) {
-                            destination = 16;
-                        }else if (board.getPlayerPosition(player_iD)==18 || board.getPlayerPosition(player_iD)== 23) {
-                            destination = 26;
+                        if(board.getPlayerPosition(playerId)==3 || board.getPlayerPosition(playerId)==37) {
+                            newPos = 6;
+                        }else if(board.getPlayerPosition(playerId)==8) {
+                            newPos = 16;
+                        }else if (board.getPlayerPosition(playerId)==18 || board.getPlayerPosition(playerId)== 23) {
+                            newPos = 26;
                         }else{ /*(board.getPlayerPosition(player_iD)==34)*/
-                            destination = 36;}
+                            newPos = 36;}
                         break;
-                    case 729: // nærmeste ræderi
-                        if (board.getPlayerPosition(player_iD) == 18 || board.getPlayerPosition(player_iD) == 23 ){
-                            destination = 29;/* Coke*/}
-                        else{
-                            destination = 13;// Squash
-                            
-                        }
-
+                    case 829: // mols-linien
+                        newPos = 16;
                         break;
                     default:
                         throw new IllegalArgumentException("Invalid ID value");
                 }
 
                 MoveToCard card = ((MoveToCard) upper);
-                // hvis det virker at tage et tilfældigt moveToCard hver gang der bliver trukket et moveToCard
+                // hvis det virker at tage et tilfældigt moveToCard hver gang der bliver trukket et moveToCard:
                 int destinationIndex = (int)(Math.random() * card.getDestination().length);
                 int randomDestination = card.getDestination()[destinationIndex];
-                //randomDestination skal så erstatte desination i næste linje
+                //randomDestination skal så erstatte newPos i næste linje
 
-                PlayerMovement moveAmount = board.generateDirectMove(player.ID, destination);
+                PlayerMovement moveAmount = board.generateDirectMove(player.ID, newPos);
                 board.performMove(player.ID,moveAmount);
 
 
             }
             else if (upper instanceof GoToJailCard){
                 GoToJailCard card = ((GoToJailCard) upper);
-
-                //jail.addPlayer(Player player.ID);
+                switch (upper.getID()) {
+                    case 40: // jail
+                        newPos = 1;
+                        break;
+                    default:
+                        throw new IllegalArgumentException("Invalid ID value");
+                }
+                PlayerMovement goToJail = board.generateDirectMove(player.ID, newPos);
+                //jail.addPlayer(Player playerId);
 
             } else if (upper instanceof MoveCard) {
                 MoveCard card = ((MoveCard) upper);
                 switch (upper.getID()) {
                     case 31:
-                        moveToField = 3;
+                        amount = 3;
                         break;
                     case 32:
-                        moveToField = -3;
+                        amount = -3;
                         break;
                 }
                 // tænker her at at card.get_amount burde kunne eleminere switch casen
                 int moveToFields = card.get_amount();
-                PlayerMovement moveAmount = board.generateForwardMove(player_iD,moveToField);
+                PlayerMovement moveAmount = board.generateForwardMove(player.ID,amount);
 
-                board.performMove(player_iD,moveAmount);
-
-
-
+                board.performMove(player.ID,moveAmount);
 
             }
             return upper;
@@ -135,6 +137,8 @@ public class ControllerChanceCard {
         }
         return instance;
     }
+
+
 
     }
     //int destination = card.getDestination()[0];
