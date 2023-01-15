e99b3803a22ef683992918bda5470f82519f252c
ZEE <Zaidsh02@gmail.com>
2023-01-15 17:09:44 +0100

    USF3 - GUISellProperty working

--- src/main/java/dtu/gruppe10/gui/GUISellProperty.java
+++ src/main/java/dtu/gruppe10/gui/GUISellProperty.java
@@ -1,11 +1,23 @@
 package dtu.gruppe10.gui;
 
+import dtu.gruppe10.Game;
+import dtu.gruppe10.board.Board;
+
 import javax.swing.*;
 
-public class GUISellProperty extends JFrame {
+public class GUISellProperty extends JFrame{
     private GUIPlayer[] players;
     private GUIBalances balances;
-    public GUISellProperty(int player, int balances){
+    private Game game;
+    private Board board;
+    public GUISellProperty(GUIPlayer[] players, GUIBalances balances){
+        super("Sell Property");
+        this.players = players;
+        this.balances = balances;
+
+        //makes a new window
+        setSize(500, 200);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
     }
 }
