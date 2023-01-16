0a57e4eb7df43dfbfda3e3a198968f8cd292e21d
Felix Schmidt <s224313@dtu.dk>
2023-01-17 01:50:19 +0800

    perHouseMoneyCard returns amount based on number of houses

--- src/main/java/dtu/gruppe10/ChanceCard/PerHouseMoneyCard.java
+++ src/main/java/dtu/gruppe10/ChanceCard/PerHouseMoneyCard.java
@@ -4,12 +4,36 @@ import dtu.gruppe10.Player;
 
 public class PerHouseMoneyCard extends MoneyCard {
     int amount;
+    int houses=0; // this will be changed to where the house
     PerHouseMoneyCard(int ID, int amount) {
         super(ID);
         this.amount = amount;
     }
+
     public int getAmount() {
         int houseMultiplier;
+        switch (houses) {
+            case 0:
+                houseMultiplier = amount;
+                break;
+            case 1:
+                houseMultiplier = amount * 2;
+                break;
+            case 2:
+                houseMultiplier = amount * 3;
+                break;
+            case 3:
+                houseMultiplier = amount * 4;
+                break;
+            case 4:
+                houseMultiplier = amount * 5;
+                break;
+            default:
+                houseMultiplier = amount * 5;
+                break;
+        }
+        return houseMultiplier;
         //housecount = getHouseCount();
         //switch (housecount)
     }
+}
