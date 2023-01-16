c67b3b5cc52acd11741a893805e2ee162546a472
Felix Schmidt <s224313@dtu.dk>
2023-01-16 16:09:15 +0800

    Made changes to Draw Method. Now only returns card values

--- src/main/java/dtu/gruppe10/ChanceCard/MoveCard.java
+++ src/main/java/dtu/gruppe10/ChanceCard/MoveCard.java
@@ -1,9 +1,14 @@
 package dtu.gruppe10.ChanceCard;
 
 public class MoveCard extends ChanceCard {
+    int amount;
     public MoveCard(int ID, int amount) {
         super(ID);
+        this.amount = amount;
 
+    }
+    public int getMoveAmount (){
+        return amount;
     }
 
 }
--- src/main/java/dtu/gruppe10/ChanceCard/MoveToCard.java
+++ src/main/java/dtu/gruppe10/ChanceCard/MoveToCard.java
@@ -1,9 +1,15 @@
 package dtu.gruppe10.ChanceCard;
 
 public class MoveToCard extends MoveCard {
+    int positionIndex;
+
     public MoveToCard(int ID, int positionIndex) {
         super(ID, positionIndex);
+        this.positionIndex = positionIndex;
 
+    }
+    public int getPosition(){
+        return positionIndex;
     }
 }
 
